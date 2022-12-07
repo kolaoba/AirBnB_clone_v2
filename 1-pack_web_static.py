7a40ac1e3a74d9be89872c91c4f36c03e3e9840f
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2022-12-07 18:20:24 +0000

    Added 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 13 14:21:54 2020
-@author: Robinson Montes
-"""
+""" Fabfile to generates a .tgz archive from the contents of web_static """
 from fabric.api import local, env
 from datetime import datetime
 
