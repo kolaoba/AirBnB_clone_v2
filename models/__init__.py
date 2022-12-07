d8795e28687650fa485fbc1a13e1910707583bc2
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-12-07 11:17:15 -0800

    updated models/amenity.py and models/__init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
 
@@ -7,4 +8,4 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()#!/usr/bin/python3
+storage.reload()
