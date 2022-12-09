f11f0f4ddb5be443cc6cc7d7e1bc5bc7005385b5
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2022-12-09 14:32:17 +0000

    updated place.py

--- models/place.py
+++ models/place.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
-from models.amenity import Amenity
-from models.review import Review
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 
 
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True,
+                             nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity",
+    reviews = relationship("Review", backref='place', cascade="all, delete")
+    amenities = relationship("Amenity", secondary=place_amenity,
                              viewonly=False)
-    amenity_ids = []
 
     @property
     def reviews(self):
@@ -52,7 +48,7 @@ class Place(BaseModel, Base):
     def amenities(self):
         """Getter attribute that returns the list of Amenity instances based on
         the attribute amenity_ids that contains all Amenity.id linked to the
-        Place
+        Place.
         """
         from models import storage
         my_list = []
@@ -65,7 +61,7 @@ class Place(BaseModel, Base):
     @amenities.setter
     def amenities(self, obj):
         """Setter attribute that handles append method for adding an Amenity.id
-        to the attribute amenity_ids
+        to the attribute amenity_ids.
         """
         if isinstance(obj, 'Amenity'):
             self.amenity_id.append(obj.id)
