ed5dc9f3ade37225d8c640d9bee09c2420872126
kolaoba <kolaobajuluwa@gmail.com>
2023-01-15 16:46:55 +0100

    added task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,10 +9,12 @@ def hello_hbnb():
     """print hello hbnb"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-     """prints hbnb"""
-     return 'HBNB'
+    """prints hbnb"""
+    return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
